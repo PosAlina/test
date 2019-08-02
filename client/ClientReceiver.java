07be04afbd68c08b321ba52a238d73c61eca20bb
Alina Pospelova <pospelowa.alina@yandex.ru>
2019-08-02 08:21:22 +0300

    Update ClientReceiver.java

--- client/ClientReceiver.java
+++ client/ClientReceiver.java
@@ -4,20 +4,21 @@ import com.db.edu.Messenger.client.clientProcessor.ClientConnector;
 import com.db.edu.Messenger.exceptions.ClientConnectionException;
 
 public class ClientReceiver {
-    private final static int PORT = 80;
-
     public static void main(String[] args) {
         try {
-            ClientConnector clientConnector = new ClientConnector(PORT);
+            ClientConnector clientConnector = new ClientConnector();
+
             try {
-            while (true) {
-                String message = clientConnector.receive();
-                clientConnector.print(message);
-            }
+                while (true) {
+                    String message = clientConnector.receive();
+                    clientConnector.print(message);
+                }
+
             } catch (ClientConnectionException e) {
-                clientConnector.closeConnection();
                 System.out.println(e.getMessage());
+                clientConnector.closeConnection();
             }
+
         } catch (ClientConnectionException e) {
             System.out.println(e.getMessage());
         }
