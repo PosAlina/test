490452f6e5faf7bf1b61df029956547d5b3f77a5
Alina Pospelova <pospelowa.alina@yandex.ru>
2019-08-02 08:20:57 +0300

    Update UserCommands.java

--- client/clientProcessor/UserCommands.java
+++ client/clientProcessor/UserCommands.java
@@ -1,9 +1,29 @@
 package com.db.edu.Messenger.client.clientProcessor;
 
 public class UserCommands {
-    public final String send = "/snd";
-    public final String name = "/chid";
-    public final String history = "/history";
-    public final String close = "/close";
-    public final String wrong = "/wrong";
+    private final String send = "/snd";
+    private final String name = "/chid";
+    private final String history = "/history";
+    private final String close = "/close";
+    private final String wrong = "/wrong";
+
+    public boolean isName(String message) { return name.equals(message); }
+
+    public boolean isSend(String message) { return send.equals(message); }
+
+    public boolean isHistory(String message) { return history.equals(message); }
+
+    public boolean isClose(String message) { return close.equals(message); }
+
+    public boolean isWrong(String message) { return wrong.equals(message); }
+
+    public String name() { return name; }
+
+    public String send() { return send; }
+
+    public String history() { return history; }
+
+    public  String  close() { return close; }
+
+    public String wrong() { return wrong; }
 }
