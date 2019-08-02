8e29e903fc0e235738486b8ba8cca39989a2d4e0
Alina Pospelova <pospelowa.alina@yandex.ru>
2019-08-02 08:22:11 +0300

    Update ClientSender.java

--- client/ClientSender.java
+++ client/ClientSender.java
@@ -2,61 +2,64 @@ package com.db.edu.Messenger.client;
 
 import com.db.edu.Messenger.client.clientProcessor.ClientConnector;
 import com.db.edu.Messenger.client.clientProcessor.ClientMessageHandler;
-import com.db.edu.Messenger.client.clientProcessor.UserCommands;
 import com.db.edu.Messenger.exceptions.ClientConnectionException;
 
 public class ClientSender {
-    private final static int PORT = 80;
+    private static ClientConnector clientConnector;
 
     public static void main(String[] args) {
         try {
-            ClientConnector clientConnector = new ClientConnector(PORT);
+            clientConnector = new ClientConnector();
 
-            createCheckerConnection(clientConnector);
-
-            createMessageReaderAndSender(clientConnector);
+            createCheckerConnection();
+            createMessageReaderAndSender();
 
         } catch (ClientConnectionException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    private static void createCheckerConnection(ClientConnector clientConnector) {
+    private static void createCheckerConnection() {
         new Thread(() -> {
             try {
                 while (true) {
                     clientConnector.checkConnection();
                 }
+
             } catch (ClientConnectionException e) {
+                System.out.println(e.getMessage());
+
                 try {
                     clientConnector.closeConnection();
+
                 } catch (ClientConnectionException ex) {
                     System.out.println(ex.getMessage());
                 }
+
             }
         }).start();
     }
 
-    private static void createMessageReaderAndSender(ClientConnector clientConnector) throws ClientConnectionException {
-        UserCommands userCommands = new UserCommands();
-        ClientMessageHandler clientMessageHanlder = new ClientMessageHandler(clientConnector, userCommands);
-
+    private static void createMessageReaderAndSender() throws ClientConnectionException {
+        ClientMessageHandler clientMessageHanlder = new ClientMessageHandler(clientConnector);
         clientMessageHanlder.authorize();
 
         while (true) {
             String message = clientConnector.read();
-            String date = clientMessageHanlder.parseDate();
 
+            String date = clientMessageHanlder.parseDate();
             message = clientMessageHanlder.parseMessage(message);
-            if (userCommands.close.equals(message)) {
+
+            if (clientMessageHanlder.isCloseMessage(message)) {
                 break;
             }
-            if (userCommands.wrong.equals(message)) {
+            if (clientMessageHanlder.isWrongMessage(message)) {
                 continue;
             }
 
             clientConnector.send(date + " " + message);
         }
+
         clientConnector.closeConnection();
     }
 }
